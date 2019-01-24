661d140d98be2c5cb640e7bb01edc51ef6205686
njudd <nickkjudd@gmail.com>
2019-01-24 12:15:50 +0100

    added vecs of vals

--- Graphs.py
+++ Graphs.py
@@ -19,7 +19,24 @@ t.columns = ["MRI1", "MRI2", "base", "MRI1_long", "MRI2_long"]
 
 # usually now I would want to average rows MRI1 & MRI2 into standard_recon
 # and MRI1_long with MRI2_long into long_recon, yet the subs don't match
-# which is somethign I need to fix in the bash script...
+# which is something I need to fix in the bash script...
+
+standard_recon = np.concatenate((np.array(t['MRI1']), np.array(t['MRI2'])), axis=0)
+standard_recon = standard_recon[~np.isnan(standard_recon)]
+
+long_recon = np.concatenate((np.array(t['MRI1_long']), np.array(t['MRI2_long'])), axis=0)
+long_recon = long_recon[~np.isnan(long_recon)]
+
+# Histograms of different processing times, with average value clearly marked for the 3 processes
+
+t['MRI1']
+
+
+
+# Scatterplot of processing time with TIV
+
+
+
 
 
 
